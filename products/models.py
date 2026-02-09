afb9a9a60282e2f55a78c5e018c53b7e9005b855
TylerRTDev <124304469+TylerRTDev@users.noreply.github.com>
2026-02-09 22:35:20 +0000

    Upgrade: - Image upload now possible for test products via admin console - Media bucket now functional and bucket policy update via docker compose (host address updated/no longer using localhost)

--- products/models.py
+++ products/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
+from config.storage_backends import MinioMediaStorageTesting
 
+media_storage = MinioMediaStorageTesting()
+
+def product_image_upload_to(instance: "Product", filename: str) -> str:
+    return f"product/{instance.name}/{filename}"
 class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
+    product_img = models.ImageField(
+        storage=media_storage,
+        upload_to=product_image_upload_to,
+        null=True,
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=["jpg", "jpeg", "png", "webp"])],
+    )
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
